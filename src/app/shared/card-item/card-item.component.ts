8800e08a659abab55606706bdbe75074da9f730d
Will_Moura <william@febrafar.com.br>
2022-02-25 18:14:05 -0300

    refctory cards skill

--- src/app/shared/card-item/card-item.component.ts
+++ src/app/shared/card-item/card-item.component.ts
@@ -7,9 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CardItemComponent implements OnInit {
   @Input() title: string;
+  @Input() src: string;
+  @Input() alt: string;
+  @Input() progress: number;
   constructor() {
-    this.title = 'William';
-   }
+    this.title = '';
+    this.src = '';
+    this.alt = '';
+    this.progress = 0;
+  }
 
   ngOnInit(): void {
   }
